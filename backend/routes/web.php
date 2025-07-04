6dcc963fe7029b2852b311d3e1ba38fd884c33ba
Ruchira Tharanka <150226449+R-Tharanka@users.noreply.github.com>
2025-07-04 23:59:27 +0530

    Remove MongoDB connection test controller and related test script

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MongoTestController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-// MongoDB connection test route
-Route::get('/mongo-test', [MongoTestController::class, 'index']);
